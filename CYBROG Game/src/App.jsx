6257779b792aca046344c4a626f922f511c3bf27
Belal-01 <belalkhobieh343@gmail.com>
2024-06-24 16:39:51 +0300

    preaparing the app and start building the game

---
+++ CYBROG Game/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header"
+import Container from "./components/Container"
+import Footer from "./components/Footer"
+import 'bootstrap/dist/css/bootstrap.min.css'
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Container />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
